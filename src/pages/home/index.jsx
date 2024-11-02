a9ddd6d824b8be630049a62bec4d03a42d60272a
SanuRaj-K <sanusrk29@gmail.com>
2024-11-02 09:35:43 +0530

    fix:fixing slider

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -51,20 +51,57 @@ const HomePage = () => {
           className="h-[500px] md:h-[800px] w-full relative overflow-hidden"
           style={{ backgroundColor: currentSlide.backgroud }}
         >
-          {/* Your Slider Content */}
+          <div className="relative mt-10 font-openSans p-3">
+            <div className="flex justify-center items-center h-[500px] md:h-[800px]">
+              <motion.h1
+                key={currentSlide.title}
+                initial={{ opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 1.5, ease: "easeInOut" }}
+                className="text-[50px] sm:text-[100px] md:text-[150px] lg:text-[200px] capitalize font-semibold text-white"
+              >
+                {currentSlide.title}
+              </motion.h1>
+            </div>
+            <motion.div
+              key={currentSlide.fruitImage}
+              initial={{ y: "100%", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: "-100%", opacity: 0 }}
+              transition={{ duration: 1, ease: "easeInOut" }}
+              className="z-20 absolute lg:top-[40px] top-[100px] md:top-[200px]"
+            >
+              <img src={currentSlide.fruitImage} alt="fruit-image" />
+            </motion.div>
+            <div className="z-20 absolute top-1/2 flex w-[100px] md:w-[200px] flex-col items-center left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <motion.img
+                key={currentSlide.cupImage}
+                src={currentSlide.cupImage}
+                alt="cup-image"
+                initial={{ y: "-100%", opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: "-100%", opacity: 0 }}
+                transition={{ duration: 1, ease: "easeInOut" }}
+              />
+              <div className="md:px-2 px-3 text-center w-[110px] md:w-full py-1 hover:bg-[#7C4995] hover:border-black transition-all duration-100 mt-12 border-[1px] cursor-pointer border-white rounded-md text-white text-[16px] md:text-[20px]">
+                <div>Order Now</div>
+              </div>
+            </div>
+          </div>{" "}
         </motion.section>
       </div>
 
       {/* Header with Dynamic Background Color */}
       <section
-        className={`${shadow && 'shadow-lg'} w-full fixed top-0  z-50`}
+        className={`${shadow && "shadow-lg"} w-full fixed top-0  z-50`}
         style={{ backgroundColor: headerColor }}
       >
         <Header />
       </section>
 
       <div className="bg-white mt-[500px] sm:mt-[500px] md:mt-[800px] w-full relative z-10">
-         <FlavoursSection />
+        <FlavoursSection />
         <ReviewSection />
         <CommunitySection />
         <Footer />
